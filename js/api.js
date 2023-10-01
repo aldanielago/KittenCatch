43c264f1d5ef94cd1603fec4697a9e92fdf52f8e
aldanielago <aldanielago@gmail.com>
2023-10-01 11:20:20 -0500

    [Funcionality] Adding favorite cats

--- js/api.js
+++ js/api.js
@@ -1,33 +1,44 @@
-const URL = "https://api.thecatapi.com/v1/images/search";
-const secret = 'live_H2wWIYYW8JqbNtD0UrO6qYC0xJxFt8WYCkt9bCgZ64nQHlKbMXjrbqBAf7XZrpbs';
+const URL = "https://api.thecatapi.com/v1";
+const secret = 'live_J9AQh9q3XIn68qL5EO2oXgDSqRL5iH4xzexuYBxSWYu1m0sPAIE4k8sy6UDdyb81';
 
-async function get_anything(url, callback, container){
-  try {
-    const res = await fetch(url);
-    const data = await res.json();
-    callback(data, container);
+async function get_anything(url, callback, container, param = 'random'){
+  const res = await fetch(url);
+  const data = await res.json();
 
-  } catch (error) {
-    const error_m = sw_error('Something went wrong at get information.', error);
+  if(res.status >= 200 || res.status < 300){
+    callback(data, container, param);
+  } else {
+    const error_m = sw_error(res.status, data.message);
     error_m();
   }
 }
 
-async function post_add_favorite_kitten(url){
-  try {
-    const res = await fetch(url, {
-      method: 'POST'
-    });
-    const data = await res.json();
-    sw_succes('Yo have added a cat to your favorite list succesfull!', data.message)
-  } catch(error){
-    const error_m = sw_error('Something went wrong at post method.', error);
+async function post_add_favorite_kitten(id){
+  const res = await fetch(`${URL}/favourites?api_key=${secret}`, {
+    method: 'POST',
+    mode: 'cors',
+		credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': secret
+    },
+    body: JSON.stringify({
+      image_id: id
+    })
+  });
+  const data = await res.json();
+
+  if(res.ok){
+    const succes_m = sw_succes('You have added a cat to your favorite list succesfull!');
+    succes_m();
+  } else {
+    const error_m = sw_error(res.status, data.message);
     error_m();
   }
 }
 
 // Resquest to the API to get images
-get_anything(`${URL}?limit=3`, append_images, container_random_images);
+get_anything(`${URL}/images/search?limit=3`, append_images, container_random_images);
 
 // Request to the API for favorite kittens
-get_anything(`${URL}?api_key=${secret}`, append_images, container_favorite_images);
+get_anything(`${URL}/favourites?api_key=${secret}`, append_images, container_favorite_images, 'favorite');
--- js/dom.js
+++ js/dom.js
@@ -1,49 +1,47 @@
-// DOM MANIPULATION //
 const $ = (id) => document.querySelector(id);
-
-// Principal page
-const paws = document.querySelectorAll('.paw');
 
 // Api requests section
 const container_random_images = $('#container-random-images');
 const container_favorite_images = $('#container-favorite-images');
 const btn_change_cat = $('.btn-change-cat');
 
-// DOM INTERACTION //
 btn_change_cat.addEventListener('click', () => {
   get_anything(`${URL}?limit=3`, append_images, container_random_images);
 });
 
 
-function append_images(data, container){
+function append_images(data, container, param){
+  console.log(data);
   container.innerHTML = '';
   data.forEach(e => {
     const mini_container = document.createElement('div');
-    mini_container.classList.add('relative')
-    const img = create_image_cat(e);
-    const btn = create_favorite_button();
+    mini_container.classList.add('relative');
+    const img = create_image_cat(e, param);
+    const btn = create_favorite_button(e);
 
-    mini_container.innerHTML += img.outerHTML;
-    mini_container.innerHTML += btn.outerHTML;
-    container.innerHTML += mini_container.outerHTML;
+    mini_container.appendChild(img);
+    mini_container.appendChild(btn);
+    container.appendChild(mini_container);
   });
 };
 
-function create_image_cat(e){
+function create_image_cat(e, param){
   const img = document.createElement('img');
-  img.src = e.url;
+  param == 'random' ? img.src = e.url : img.src = e.image.url
   img.alt = e.id;
   img.classList.add('img_cat');
   return img;
 }
 
-function create_favorite_button(){
+function create_favorite_button(e){
   const btn = document.createElement('button');
   const img = document.createElement('img');
   img.src = './assets/suit-heart.svg';
   btn.classList.add('btn_dislike')
 
+  btn.addEventListener('click', () => {
+    post_add_favorite_kitten(e.id);
+  })
   btn.appendChild(img);
-  btn.addEventListener('click', post_add_favorite_kitten)
   return btn;
 }
