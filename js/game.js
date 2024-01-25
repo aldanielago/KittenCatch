77154ffd1e96d5a69d7c3eb3952c18176daef08b
aldanielago <aldanielago@gmail.com>
2024-01-25 15:47:32 -0500

    [Functionality] Add cute music

--- js/game.js
+++ js/game.js
@@ -17,6 +17,15 @@ const gameContainer = $('.counter-during-game');
 // End game
 const container_end_game = $('.container-after-game');
 
+// Music
+const music_before_game = $('#music-before-game');
+const music_during_game = $('#music-during-game');
+const music_after_game = $('#music-after-game');
+const music_countdown = $('#music-countdown');
+const meow = $('#meow1');
+
+music_before_game.play();
+
 function startGame() {
   welcomePage.classList.add('go-up');
   setTimeout(() => {
@@ -29,6 +38,8 @@ function startGame() {
 }
 
 function counterBeforeGame() {
+  music_before_game.pause();
+  music_countdown.play();
   reverseCounter.textContent = 3;
   setTimeout(() => {
     reverseCounter.textContent = 2;
@@ -41,12 +52,15 @@ function counterBeforeGame() {
     reverseCounter.textContent = 'GO';
   }, 3000);
   setTimeout(() => {
+    countainer_before_game.classList.add('disappear');
     countainer_before_game.classList.add('none');
     counterDuringGame();
+    music_countdown.pause();
   }, 4000);
 }
 
 function counterDuringGame() {
+  music_during_game.play();
   let time = 10;
   const timer = setInterval(() => {
     if (time === 0) {
@@ -61,11 +75,18 @@ function counterDuringGame() {
 
 function endGame() {
   container_end_game.classList.remove('none');
+  music_during_game.pause();
   setTimeout(() => {
+    music_after_game.play();
     container_end_game.classList.add('none');
-    phoneSection.classList.add('go-down');
-    phoneSection.classList.add('none');
+    phoneSection.classList.remove('go-down');
+    phoneSection.classList.add('go-up-phone');
   }, 2000);
+  setTimeout(() => {
+    phoneSection.classList.add('none');
+    music_after_game.pause();
+    music_before_game.play();
+  }, 2900);
 }
 
 let count = 0;
@@ -73,5 +94,6 @@ paws.forEach(paw => {
   paw.addEventListener('click', () => {
     count++;
     score.textContent = count;
+    meow.play();
   });
 });
